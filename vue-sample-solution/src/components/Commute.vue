b213fc4b1bead550fc8d22899d76977b319db039
Maria Husmann <maria.husmann@gmail.com>
2020-10-16 14:37:12 +0200

    Sample solution for vue

---
+++ vue-sample-solution/src/components/Commute.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="commute">
+    <h2>
+      {{person}}'s commute
+    </h2>
+    <p>
+      This is <span class="person">{{person}}</span>
+    </p>
+    <p>
+      {{person}} commutes from {{start}} to {{destination}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Commute",
+  props: {
+    person: String,
+    start: String,
+    destination: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.person {
+  color: rgb(103, 189, 189);
+  font-weight: bold;;
+}
+
+img {
+  max-width: 600px;
+}
+</style>
---
+++ vue-sample-solution/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <li v-for="commute in commutes" :key="commute.person">
+      <Commute :person="commute.person" :start="commute.start" :destination="commute.destination" />
+    </li>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Commute from "@/components/Commute.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Commute
+  },
+  data: function() {
+    return {
+      commutes: [
+        {
+          person: "Maria",
+          start: "Baar",
+          destination: "Emmenbrücke"
+        },
+        {
+          person: "Hanna",
+          start: "Zürich",
+          destination: "Emmenbrücke"
+        }
+      ]
+    };
+  }
+};
+</script>
